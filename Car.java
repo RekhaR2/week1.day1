adbdc0fee9c32887949d74158df32ac5282a3580
RekhaR2 <98229488+RekhaR2@users.noreply.github.com>
2022-01-22 23:06:17 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		
+	//Program for Primitive data type and non primitive datatype
+	
+byte regnNumber = 55;
+short seatingCapacity = 5;
+int insuranceNumber = 1234;
+long chasisNumber = 34564555545466L;
+float fuelCapacity = 124.43F;
+double engineOilCapacity = 234567.56;
+boolean isTyrePuncture = true;
+char varient = 'p';
+String carName = "Celerio";
+System.out.println("Register number = "+regnNumber);
+System.out.println("Seating Capacity= "+seatingCapacity);
+System.out.println("Insurance number= "+insuranceNumber);
+System.out.println("Chasis Number = "+ chasisNumber);
+System.out.println("Fuel capacity = "+ fuelCapacity);
+System.out.println("Engine oil capacity = "+engineOilCapacity);
+System.out.println("Is true Puncture = "+isTyrePuncture);
+System.out.println("Varient = "+varient);
+System.out.println("Car Name = "+carName);
+	}
+
+}

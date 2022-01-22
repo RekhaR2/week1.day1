ddf72af5281815a6e4c7ce0d00d325fb58dccf62
RekhaR2 <98229488+RekhaR2@users.noreply.github.com>
2022-01-23 00:31:15 +0530

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,8 +1,8 @@
 package week1.day1;
 
 public class Mobile {
-	// Program No 3  "Creating class Mobile and Call all the methods"
-	String contactName = "Rekha";
+	// Program No 3  "Creating class Mobile and Calling all the methods"
+	//String contactName = "Rekha";
 	public void sendMsg()
 	{
 		System.out.print(" Welcome to TestLeaf");
@@ -17,11 +17,12 @@ public class Mobile {
 		System.out.println("Name "+name);
 	}
 	public static void main(String[] args) {
+		String contactName = "Rekha";//Local Variable
 		Mobile obj2 = new Mobile();
-		obj2.saveContact(obj2.contactName);
+		obj2.saveContact(contactName);
 		long number = obj2.makeCall();
 		System.out.println("Mobile number "+number);
-		System.out.print("Hi "+obj2.contactName);
+		System.out.print("Hi "+contactName);
 		obj2.sendMsg();
 
 
